6b67ccdb7e4f95942a007093b774cb4a1c55a172
Guilherme <guilherme.medeiros1@escolar.ifrn.edu.br>
2026-07-16 20:06:48 -0300

    Adição dos requisitos pro flask-login

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_login import LoginManager
 from db import db, Usuario, Tarefa
 
 app = Flask(__name__)
 app.secret_key = 'chave_secreta'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -10,6 +15,10 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
 
 @app.route('/')
 def index():
--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class Usuario(db.Model):
+class Usuario(db.Model, UserMixin):
     __tablename__ = "usuarios"
 
     id = db.Column(db.Integer, primary_key=True)
